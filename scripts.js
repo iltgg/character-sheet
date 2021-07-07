e48d5945e361e3281a7c2e35186f1e8513fcc2e9
iltgg <benlow999@gmail.com>
2021-07-07 00:36:57 -0400

    starting formating

---
+++ scripts.js
@@ -0,0 +1,36 @@
+let character = {
+    name: '',
+    type: '',
+    class: '',
+    apprehension: 0,
+    wounds: {
+        fatal: [''],
+        dangers: ['', ''],
+        pains: ['', '', ''],
+        distractions: ['', '', '', ''],
+        annoyances: ['', '', '', '', '']
+    },
+    stats: {
+        strength: 0,
+        dexterity: 0,
+        constitution: 0,
+        intelligence: 0,
+        wisdom: 0,
+        charisma: 0,
+    },
+    equipment: '',
+    peculiarityPoints: 0,
+    statisticalTraining: {
+        strength: 0,
+        dexterity: 0,
+        constitution: 0,
+        intelligence: 0,
+        wisdom: 0,
+        charisma: 0,
+    },
+    peculiarityTraining: 0,
+    recoveringWound: '',
+    weeksRequired: 0,
+    skills: [[false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]]
+};
